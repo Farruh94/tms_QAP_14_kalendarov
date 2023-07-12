d74d6248f924bc7860be34be94f4126bc65a3b39
Farruh Kalendarov <farruh.kalendarov@gmail.com>
2023-07-12 11:01:32 +0300

    Home Work 7 without extra task

--- hw_7/gen_file.py
+++ hw_7/gen_file.py
@@ -1,15 +1,21 @@
-# 1 Напишите генератор который принимает список
-# numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7] и
-# возвращает новый список только с положительными
-# числами
+"""
+1 Напишите генератор, который принимает список
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7] и
+возвращает новый список только с положительными
+числами
+"""
 
 
 
 def pos_number():
+    """
+    Generator that return only positive numbers
+    """
     numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
-    for i in numbers:
-        if i > 0:
-            yield i
+    for number in numbers:
+        if number > 0:
+            yield number
+
 
 positive_list = []
 for i in pos_number():
@@ -18,34 +24,37 @@ for i in pos_number():
 print(positive_list)
 
 
-
 # 2 Необходимо составить список чисел которые
 # указывают на длину слов в строке: sentence = " thequick
 # brown fox jumps over the lazy dog", но только если слово
 # не "the" с обработкой исключений
 
 class MyError(Exception):
+    """
+    My own exception
+    """
     def __init__(self, text):
         self.txt = text
 
 
-
-def findLen():
-    str = "thequick brown fox jumps over the lazy dog"
-    some_lst = iter(str.split(" "))
-    random_str = 'the'
-    word_len = 0
-    for i in some_lst:
+def find_len():
+    """
+    Measure len of words on sentence
+    """
+    sentence = "thequick brown fox jumps over the lazy dog"
+    some_lst = iter(sentence.split(" "))
+    exception_str = 'the'
+    for word in some_lst:
         try:
-            if random_str in i:
-                raise MyError(f"В слове, {i} содержится '{random_str}'")
+            if exception_str in word:
+                raise MyError(f"В слове, {word} содержится '{exception_str}'")
             else:
-                word_len = len(i)
-                yield word_len, i
+                word_len = len(word)
+                yield word_len, word
 
         except MyError as my_error:
             print(my_error)
 
 
-for j in findLen():
+for j in find_len():
     print(j, end="\n")
